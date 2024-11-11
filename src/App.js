a72b80d1626ad167e8c30cfa7e752e5ac42739b7
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-11-11 10:07:15 +0530

    Set up routing with React Router in App.js for navigation between pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Projects from './Pages/Projects';
+import Skills from './Pages/Skills';
+import Experience from './Pages/Experience';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
